d35f1058fc2256318eaf064f000398263cec35ef
Lahav Harary <123938862+LahavFibonatix@users.noreply.github.com>
2024-12-22 12:19:33 +0200

    Delete API directory

--- API/DTOs/SchoolUpdateDto.cs
+++
@@ -1,9 +0,0 @@
-namespace API.DTOs;
-
-public class SchoolUpdateDto
-{
-    public  string? Name { get; set; }
-    public  int DistrictId { get; set; } 
-    public  int LicenseId { get; set; }
-    public string? ExpiredAt { get; set; }
-}
