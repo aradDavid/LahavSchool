8ef59a99060a1b6a36aeedde7ff208d7989aef5a
aradDavid <arad.david@fibonatix.com>
2024-12-22 12:13:12 +0200

    Remove ignored files from tracking

--- API/DTOs/SchoolDto.cs
+++
@@ -1,8 +0,0 @@
-namespace API.DTOs;
-
-public class SchoolDto
-{
-    public required string Name { get; set; }
-    public required int DistrictId { get; set; } 
-    public required int LicenseId { get; set; }
-}
